d1897c432ace2645b5dc0eb9b31ac9394e8eec8e
sugimori kouta <kota.1412@icloud.com>
2023-08-12 15:24:14 +0900

    クソアプリ5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-Image.create!(title: 'イケメン高校生', body: 'おめでとう🥳', image: '1.jpeg')
+Image.create!(title: 'イケメン高校生', body: 'おめでとう🥳', image: '1.png')
 Image.create!(title: 'フ○ントガラス男', body: '残念💩', image: '2.png')
 Image.create!(title: 'おじさん', body: '残念🤓', image: '3.png')
