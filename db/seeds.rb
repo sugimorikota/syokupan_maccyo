ab55c1e8cb18521ff84d54460410cd825bf42f2a
sugimori kouta <kota.1412@icloud.com>
2023-08-13 18:46:10 +0900

    マッチョ4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-#Image.create!(title: '細マッチョ', body: '見た目は細いが、しっかりと鍛えられているマッチョ', image: '1.png')
-#Image.create!(title: 'ゴリマッチョ', body: '非常にたくましく、頼れるマッチョ', image: '2.png')
-#Image.create!(title: 'アスリートマッチョ', body: '見た目がカッコよく、動きやすさを求めたマッチョ', image: '3.png')
-#Image.create!(title: '極マッチョ', body: '人間離れした体を持つ、最強のマッチョ', image: '4.png')
+Image.create!(title: '細マッチョ', body: '見た目は細いが、しっかりと鍛えられているマッチョ', image: '1.png')
+Image.create!(title: 'ゴリマッチョ', body: '非常にたくましく、頼れるマッチョ', image: '2.png')
+Image.create!(title: 'アスリートマッチョ', body: '見た目がカッコよく、動きやすさを求めたマッチョ', image: '3.png')
+Image.create!(title: '極マッチョ', body: '人間離れした体を持つ、最強のマッチョ', image: '4.png')
